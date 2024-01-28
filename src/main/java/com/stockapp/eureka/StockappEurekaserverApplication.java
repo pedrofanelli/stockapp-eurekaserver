3a74320d6a4495cc6c5eeb64b63c2dcf71b70ffc
Pedro Fanelli <pedrofanelli1@gmail.com>
2024-01-28 12:12:18 -0300

    init

---
+++ src/main/java/com/stockapp/eureka/StockappEurekaserverApplication.java
@@ -0,0 +1,13 @@
+package com.stockapp.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockappEurekaserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockappEurekaserverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stockapp/eureka/StockappEurekaserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stockapp.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockappEurekaserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
